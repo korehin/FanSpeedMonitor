d888821c55da4dda863eabf53e7f8afc528a8bfe
korehin <korehin@gmail.com>
2026-05-17 22:15:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-namespace FanSpeedMonitor
+using System;
+using System.Windows.Forms;
+
+namespace MSI_Claw_Fan_PRM
 {
     internal static class Program
     {
@@ -6,6 +9,7 @@ namespace FanSpeedMonitor
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+
             Application.Run(new MainForm());
         }
     }
